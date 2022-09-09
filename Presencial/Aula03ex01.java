dd412840abacb813f7a86b690bd4b9aa708d1844
Felipe Schmitz <aryastarklinda@outlook.com>
2022-09-09 12:23:38 -0300

    exercicio do tempo e maior de 3

---
+++ Presencial/Aula03ex01.java
@@ -0,0 +1,38 @@
+package aula03;
+
+import java.util.Scanner;
+
+public class Aula03 {
+
+    public static void main(String[] args) {
+        
+        Scanner leitor = new Scanner (System.in);
+        
+        int hora1, min1, hora2, min2;
+        
+        System.out.println("Digite a primeira hora: ");
+        hora1 = leitor.nextInt();
+        System.out.println("Digite o primeiro minuto: ");
+        min1 = leitor.nextInt();
+        System.out.println("Digite a segunda hora: ");
+        hora2 = leitor.nextInt();
+        System.out.println("Digite o segundo minuto: ");
+        min2 = leitor.nextInt(); 
+        
+        /* hora1 = 12;
+        min1 = 45;
+        hora2 = 14;
+        min2 = 40; */
+        
+        int minutos1 = hora1 * 60 + min1;
+        int minutos2 = hora2 * 60 + min2;
+        
+        int minutosTotal = minutos2 - minutos1;
+        
+        int horaTotal = minutosTotal /60 ;
+        int minutosSobrados = minutosTotal % 60;
+        
+        System.out.println("O tempo total decorrido foi de: " + horaTotal + ":" + minutosSobrados);
+    }
+    
+}
---
+++ Presencial/Aula03ex02.java
@@ -0,0 +1,27 @@
+
+import java.util.Scanner;
+
+public class maiorDe3 {
+    
+    public static void main (String args []) {
+        Scanner leitor = new Scanner (System.in);
+        
+        int numero1, numero2, numero3;
+        
+        System.out.println("Digite o seu primeiro número");
+        numero1 = leitor.nextInt();
+        System.out.println("Digite o seu segundo número");
+        numero2 = leitor.nextInt();
+        System.out.println("Digite o seu terceiro número");
+        numero3 = leitor.nextInt();
+        
+        if (numero1 > numero2 && numero1 > numero3) {
+            System.out.println("O maior número foi o primeiro digitado: " + numero1);
+        } else if ( numero2 > numero3 ) {
+            System.out.println("O maior número foi o segundo digitado: " + numero2);
+        } else {
+            System.out.println("O maior número foi o terceiro digitado: " + numero3);
+        }
+        
+    }
+}
